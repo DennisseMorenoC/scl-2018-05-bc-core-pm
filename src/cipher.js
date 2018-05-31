808d56f5fc6448a2cc3a05cd44a6973c8e63b450
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-05-31 16:25:48 -0400

    Avance js

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,19 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+/*window.cipher = {
+  encode: (normalFrase, spaceNumber) => {
+
+    let codeFrase = "";
+            
+    for (i = 0 ; i < normalFrase.length; i++){
+      const form = (normalFrase.toUpperCase().charCodeAt(i)-65+spaceNumber)%26+65;
+      const codeFrase = String.fromCharCode(form); 
+      
+      console.log(codeFrase);
+    }
+    return codeFrase = document.getElementById("messageCode1").innerHTML;
+
   },
+
   decode: () => {
-    /* Acá va tu código */
+    /* Acá va tu código 
   }
-}
+}*/
