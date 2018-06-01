7d943edcc95e6fb93664b241722ab386d8911728
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-06-01 15:31:15 -0400

    js terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,9 @@
-/*window.cipher = {
-  encode: (normalFrase, spaceNumber) => {
-
-    let codeFrase = "";
-            
-    for (i = 0 ; i < normalFrase.length; i++){
-      const form = (normalFrase.toUpperCase().charCodeAt(i)-65+spaceNumber)%26+65;
-      const codeFrase = String.fromCharCode(form); 
+window.cipher = {
+  encode: () => {
       
-      console.log(codeFrase);
-    }
-    return codeFrase = document.getElementById("messageCode1").innerHTML;
-
   },
 
   decode: () => {
-    /* Acá va tu código 
+    
   }
-}*/
+}
