a9cae7abadb8ca101f5d8465b9df90671e25786e
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-06-18 11:40:49 -0400

    Separando cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,39 @@
 window.cipher = {
-  encode: () => {
-      
+  encode: (normalFrase, spaceNumber) => {
+    let codeFrase = "";
+            
+    for (i = 0 ; i < normalFrase.length; i++){
+        let upperCase = normalFrase.toUpperCase();
+         if(upperCase.charCodeAt(i) == 32){
+            codeFrase += String.fromCharCode(upperCase.charCodeAt(i));
+        }else{
+            let form = (upperCase.charCodeAt(i)-65+spaceNumber)%26+65;
+            codeFrase += String.fromCharCode(form); 
+        }
+        //let form = (normalFrase.toUpperCase().charCodeAt(i)-65+spaceNumber)%26+65;
+        //codeFrase += String.fromCharCode(form); 
+        //console.log(codeFrase);
+    }  
+    return codeFrase;
   },
 
-  decode: () => {
+  decode: (insertCodeFrase, spaceNumber) => {
+    let decodeFrase = "";
+    
+    for (i = 0; i<insertCodeFrase.length; i++){
+        let upperCase2 = insertCodeFrase.toUpperCase();
+
+        if (upperCase2.charCodeAt(i) == 32){
+            decodeFrase += String.fromCharCode(upperCase2.charCodeAt(i));
+
+        }else{
+            let form2 = (upperCase2.charCodeAt(i)+65-spaceNumber)%26+65;
+            decodeFrase += String.fromCharCode(form2);
+        }
+        //let form2 = (insertCodeFrase.toUpperCase().charCodeAt(i)+65-spaceNumber)%26+65;
+        //    decodeFrase += String.fromCharCode(form2);
+        //console.log(decodeFrase);   
+    }
     
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,54 +1,20 @@
 function encode (){
     let normalFrase = document.getElementById("messageNatural1").value;
-    console.log(normalFrase);
     let spaceNumber = parseInt(document.getElementById("llave").value);
-    console.log(spaceNumber);
+    let codeFrase = window.cipher.encode(normalFrase, spaceNumber);
+    document.getElementById("messageCode1").innerHTML = codeFrase;
+
     //let codeFrase = windows.cipher.encode(normalFrase, spaceNumber);
     //document.getElementById("messageCode1").innerHTML;
-    let codeFrase = "";
-            
-    for (i = 0 ; i < normalFrase.length; i++){
-        let upperCase = normalFrase.toUpperCase();
-        if(upperCase.charCodeAt(i) == 32){
-            codeFrase += String.fromCharCode(upperCase.charCodeAt(i));
-            document.getElementById("messageCode1").innerHTML = codeFrase;
-                    
-        }else{
-            let form = (upperCase.charCodeAt(i)-65+spaceNumber)%26+65
-            codeFrase += String.fromCharCode(form); 
-            document.getElementById("messageCode1").innerHTML = codeFrase;
-            
-        }
-        //let form = (normalFrase.toUpperCase().charCodeAt(i)-65+spaceNumber)%26+65;
-        //codeFrase += String.fromCharCode(form); 
-        //console.log(codeFrase);
-    }
+ 
     //document.getElementById("messageCode1").innerHTML = codeFrase;
 }
 
 function decode() {
     let insertCodeFrase = document.getElementById("messageCode2").value;
     let spaceNumber = parseInt(document.getElementById("llave").value);
-    let decodeFrase = "";
-    
-    for (i = 0; i<insertCodeFrase.length; i++){
-        let upperCase2 = insertCodeFrase.toUpperCase();
-
-        if (upperCase2.charCodeAt(i) == 32){
-            decodeFrase += String.fromCharCode(upperCase2.charCodeAt(i));
-            document.getElementById("messageNatural2").innerHTML = decodeFrase;
-
-        }else{
-            let form2 = (upperCase2.charCodeAt(i)+65-spaceNumber)%26+65;
-            decodeFrase += String.fromCharCode(form2);
-            document.getElementById("messageNatural2").innerHTML = decodeFrase;
-            
-        }
-        //let form2 = (insertCodeFrase.toUpperCase().charCodeAt(i)+65-spaceNumber)%26+65;
-        //    decodeFrase += String.fromCharCode(form2);
-        //console.log(decodeFrase);   
-    }
-    
+    let decodeFrase = window.cipher.encode(insertCodeFrase, spaceNumber);
+    document.getElementById("messageNatural2").innerHTML = decodeFrase;
     //document.getElementById("messageNatural2").innerHTML = decodeFrase;
 
 }
